5e2aa94489fd1dffdb559728f0149f5c902639c6
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2014-09-14 20:48:13 -0400

    Initial commit for.
    This commit is just to add the files to github to make sure I can access them from everywhere I want.

---
+++ common.h
@@ -0,0 +1,27 @@
+/*
+ * common.h
+ *
+ *  Created on: Sep 12, 2014
+ *      Author: fathi
+ */
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#define DEBUG 1
+
+#define log_debug(msg)                       \
+if (DEBUG) {                                 \
+    printf("[DEBUG]: ");                     \
+    printf((msg));                           \
+    printf("\n");                            \
+}                                            \
+
+#define log_debug2(msg, code)                \
+if (DEBUG) {                                 \
+    printf("[DEBUG]: ");                     \
+    printf((msg), (code));                   \
+    printf("\n");                            \
+}                                            \
+
+#endif /* COMMON_H_ */

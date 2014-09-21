43db3ca470368c9c4d2678d38b91125c7773ebc4
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2014-09-21 18:59:25 -0400

    Adding the first sanity test case to make sure CHECK works

--- test/check_common.c
+++ test/check_common.c
@@ -5,11 +5,40 @@
  *      Author: fathi
  */
 
-
+#include <stdlib.h>
 #include <check.h>
 
-START_TEST ("Read file")
-{
+void setup(void) {
+    // don't do anything
+}
 
+void teardown(void) {
+    // don't do anything
+}
+
+START_TEST (sanity_chech) {
+        fail_if(1 != 1);
 }
 END_TEST
+
+Suite *common_suite(void) {
+    Suite *s = suite_create("Common");
+
+    /* Core test case */
+    TCase *tc_core = tcase_create("Core");
+    tcase_add_checked_fixture(tc_core, setup, teardown);
+    tcase_add_test(tc_core, sanity_chech);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
+
+int main(void) {
+    int number_failed;
+    Suite *s = common_suite();
+    SRunner *sr = srunner_create(s);
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
